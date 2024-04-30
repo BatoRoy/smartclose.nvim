1221f6d8dd94ba35f1ceb4d539471fd346ad7e8c
BatoRoy <kaptenroy00@gmail.com>
2024-04-30 22:17:24 +0200

    Added licence info and disabled smart close on <

--- lua/smartclose/init.lua
+++ lua/smartclose/init.lua
@@ -1,4 +1,30 @@
 
+-- MIT License
+
+-- Copyright (c) 2024 Adam Westergren
+
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+
+-- in the Software without restriction, including without limitation the rights
+
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 function string.insert(str1, str2, pos)
     return str1:sub(1, pos) .. str2 .. str1:sub(pos + 1)
 end
@@ -26,10 +52,13 @@ function _GetNextChar(current_line, col)
 
     -- Search current line and add to stack.
 
+    local all_characters = "[%(%[{<%>}%]%)\"\']"
+    local normal_characters = "[%(%[{}%]%)\"\']"
+
     local single_quote_opened = false
     local double_quote_opened = false
     local pos = 1
-    for c in current_line:gmatch("[%(%[{<%>}%]%)\"\']") do
+    for c in current_line:gmatch(normal_characters) do
         local start_pos, end_pos = current_line:find(c, pos, true)
         local escape1_start_pos = current_line:find('\\', start_pos - 1, true)
         local escape2_start_pos = current_line:find('%', pos, true)
--- tests/smartclose_spec.lua
+++ tests/smartclose_spec.lua
@@ -1,3 +1,30 @@
+
+-- MIT License
+
+-- Copyright (c) 2024 Adam Westergren
+
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+
+-- in the Software without restriction, including without limitation the rights
+
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 describe("smartclose", function ()
     it("can be required", function ()
         require("smartclose")
@@ -15,10 +42,10 @@ describe("smartclose", function ()
         local c = require("smartclose")._GetNextChar("{", 100)
         assert.are.same(c, "}")
     end)
-    it("can close <", function ()
-        local c = require("smartclose")._GetNextChar("<", 100)
-        assert.are.same(c, ">")
-    end)
+    -- it("can close <", function ()
+    --     local c = require("smartclose")._GetNextChar("<", 100)
+    --     assert.are.same(c, ">")
+    -- end)
     it("can close \"", function ()
         local c = require("smartclose")._GetNextChar("\"", 100)
         assert.are.same(c, "\"")
@@ -59,5 +86,4 @@ describe("smartclose", function ()
         local c = require("smartclose")._GetNextChar("\"(\"", 100)
         assert.are.same(c, ")")
     end)
-    
 end)
